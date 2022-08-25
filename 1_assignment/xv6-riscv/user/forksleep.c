013392daf279552bb8e8f9c0cf62578021a2eb2f
pmaniar20 <pmaniar20@iitk.ac.in>
2022-08-25 18:09:44 +0530

    added mkfs

--- 1_assignment/xv6-riscv/user/forksleep.c
+++ 1_assignment/xv6-riscv/user/forksleep.c
@@ -6,9 +6,21 @@ int main(int argc, char *argv[]){
     m = atoi(argv[1]);
     n = atoi(argv[2]);
     int * a = (int*)malloc(sizeof(int));
-
-    if(n == 0){     
-        if(fork()==0){
+    if(m<=0){
+        printf("m must be positive\n");
+        exit(0);
+    }
+    if(n!=0 && n!=1){
+        printf("n must be 0 or 1\n");
+        exit(0);
+    }
+    if(n == 0){  
+        int x=fork();
+        if(x<0){
+            printf("fork failed\n");
+            exit(0);
+        }
+        else if(x==0){
             sleep(m);
             printf("%d: Child\n", getpid());
         }
@@ -17,8 +29,13 @@ int main(int argc, char *argv[]){
             wait(a);
         }
     }
-    else{                
-        if(fork()==0){
+    else{     
+        int x=fork();
+        if(x<0){
+            printf("fork failed\n");
+            exit(0);
+        }
+        else if(x==0){
             printf("%d: Child\n", getpid());
         }
         else{
